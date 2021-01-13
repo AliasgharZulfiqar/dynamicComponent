7733bee8d143f2ccd2d864c362a354cb19ab9da0
aliasghar <ali.asghar@10pearls.com>
2021-01-13 17:25:01 +0500

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,70 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
+import { Component } from "@angular/core";
+import {
+  FormBuilder,
+  FormControl,
+  FormControlName,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  genComp :boolean = true;
-  title = 'dynamicComp';
-  formattedText : string = '';
-  dynamicForm : FormGroup;
-  formObj : FormGroup;
-  testForm : FormGroup;
-  test :FormControlName
-  compForm : FormGroup;
-  constructor(private fb : FormBuilder){
-  this.dynamicForm = this.fb.group({
-    formattedText : ['<form [formGroup]="testForm"><input type="text" formControlName="test"/><input type="number" maxLength=10 formControlName="test"/></form>']
-  })
+  genComp: boolean = true;
+  title = "dynamicComp";
+  formattedText: string = "";
+  dynamicForm: FormGroup;
+  formObj: FormGroup;
+  testForm: FormGroup;
+  test: FormControlName;
+
+  compForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.dynamicForm = this.fb.group({
+      formattedText: ["", [Validators.required]],
+      // <form [formGroup]="testForm"><input type="text" formControlName="test"/><input type="number" maxLength=10 formControlName="test"/></form>
+    });
   }
-  formSubmission(data){
-    console.log(data);
+  formSubmission(data) {
+    // console.log(data);
   }
-  genComponent(){
-    this.genComp = true;
+  createForm() {
+    if (this.dynamicForm.get("formattedText").value == null) {
+      this.dynamicForm
+        .get("formattedText")
+        .setValue(
+          '<form [formGroup]="demoForm"> <input type="text" formControlName="testInput"/> <input type="text" formControlName="secondTestInput"/> <input type="text" formControlName="thirdTestInput"/></form>'
+        );
+    } else {
+      let value = this.dynamicForm.get("formattedText").value;
+      value +='<form [formGroup]="demoForm"> <input type="text" formControlName="testInput"/> <input type="text" formControlName="secondTestInput"/> <input type="text" formControlName="thirdTestInput"/></form>';
+      this.dynamicForm.get("formattedText").setValue(value);
+    }
+    let demoForm = new FormGroup({
+      testInput: new FormControl(""),
+      secondTestInput: new FormControl(""),
+      thirdTestInput: new FormControl(""),
+    });
+    this.formObj = demoForm;
   }
-  removeComponent(){
-    this.genComp = false;
-  }
-  getFormData(){
+  // genComponent(){
+  //   this.genComp = true;
+  // }
+  // removeComponent(){
+  //   this.genComp = false;
+  // }
+  getFormData() {
     this.formattedText = this.dynamicForm.value.formattedText;
-  //  this.dynamicForm.addControl('testing',new FormControl(''))
-   console.log(this.dynamicForm)
-   this.compForm = this.dynamicForm;
+    //  this.dynamicForm.addControl('testing',new FormControl(''))
+    // console.log(this.dynamicForm)
+    this.compForm = this.dynamicForm;
   }
 
-  getMessage(data){
-    alert(data)
+  getMessage(data) {
+    alert(data);
   }
   // updateElement(){
   //   this.dynamicForm.get('testing').setErrors({required:true});
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -13,38 +13,30 @@ import { DynamicComponent } from '../dynamic/dynamic.component';
 })
 export class ChildComponent implements OnInit, OnChanges {
   @Input() formattedText: string;
-  // @Input() testForm  : FormGroup;
+  @Input() demoForm: FormGroup;
   @ViewChild("insert", { read: ViewContainerRef }) vf: ViewContainerRef;
-  @Input() testingForm : FormGroup;
+  @Input() testingForm: FormGroup;
   @Output() fireMessage: EventEmitter<any> = new EventEmitter();
+  // @Input() testForm  : FormGroup;
   //This is Dynamic Rendering
   //@ViewChild('second',{read:ViewContainerRef}) svf : ViewContainerRef;
-  compRef;
-  genCom;
+  // compRef;
+  // genCom;
   constructor(
     private componentFactoryResolver: ComponentFactoryResolver,
     private compiler: Compiler
-  ) {
-  }
+  ) {}
   ngOnChanges(changes: SimpleChanges): void {
-    
-    this.generateDynamicComp(this.formattedText, this.fireMessage,this.testingForm);
+    this.generateDynamicComp(
+      this.formattedText,
+      this.fireMessage,
+      this.demoForm
+    );
 
-    // if(changes.callComp.currentValue){
-    //   this.callComp = changes.callComp.currentValue;
-
-    //   let secondComp = this.componentFactoryResolver.resolveComponentFactory(DynamicComponent);
-    //  this.compRef= this.vf.createComponent(secondComp);
-    //  //this.compRef.instance.formattedText = 'This is test <element> textInput name="firstInput" required="true" maxLength="4" minLength="2"</element>'
-    //  this.compRef.instance.value = 1;
-    //   this.compRef.instance.throwVal.subscribe(response=>{alert(response)})
-
-    // }else{
-    // this.compRef.destroy();
-    //}
   }
-
+  
   ngOnInit() {
+    // console.log(this.demoForm)
     //This is Dynamic Rendering . . .
     // let secondComp = this.componentFactoryResolver.resolveComponentFactory(DynamicComponent);
     // this.compRef= this.svf.createComponent(secondComp);
@@ -52,77 +44,92 @@ export class ChildComponent implements OnInit, OnChanges {
     // this.compRef.instance.value = 1;
     //  this.compRef.instance.throwVal.subscribe(response=>{alert(response)})
   }
-  //This function generates a dynamic component using JIT Compiler on run time . . .
-  generateDynamicComp(value: string, emitter: EventEmitter<any>,compForm : FormGroup) {
-    const componentOnDemand: any = Component({
+  //This function generates a dynamic component using JIT . . .
+  generateDynamicComp(
+    value: string,
+    emitter: EventEmitter<any>,
+    compForm: FormGroup
+  ) {
+    const componentOnDemand = Component({
       template: value,
     })(
-      class componentOnDemand implements OnInit,OnChanges {
-        testForm :FormGroup;
-        constructor() {
-          this.setupFormGroup();
+      class componentOnDemand implements OnInit {
+        demoForm: FormGroup;
+        controlsArray;
 
-        }
-        ngOnChanges(){
-       
+        constructor() {
+          this.setupForm();
         }
         ngOnInit() {
-         
-      
-       
-          emitter.emit("Fired from dynamic component " + value);
-         
+          // emitter.emit("Fired from dynamic component " + value);
         }
-        setupFormGroup(): void {
-          this.testForm = compForm
-          this.validateForm(this.testForm)
-//           if (this.testForm) {
-//             this.validateForm(this.testForm);
-//             this.onValueChanges();
-        
-         }
-       
-
-        validateForm(form: any): boolean {
-          for (const name in form.controls) {
-            if (form.controls[name] instanceof FormArray) {
-              form.controls[name].controls.forEach((formGroup: FormGroup) => {
-                this.validateForm(formGroup);
-              });
-              this.validateForm(form.controls[name]);
-            } else if (form.controls[name] instanceof FormGroup) {
-              this.validateForm(form.controls[name]);
-            } else if (form.controls[name] && form.controls[name].enabled) {
-              form.controls[name].updateValueAndValidity();
-              form.controls[name].markAsTouched();
-              form.controls[name].markAsDirty();
-            }
+        setupForm(): void {
+          this.demoForm = new FormGroup({});
+          for (let controlName in compForm.controls) {
+            this.demoForm.addControl(controlName, new FormControl());
           }
-          console.log(form);
-          return form.valid;
+          
         }
-
-//         onValueChanges(): void {
-// this.testForm.valueChanges.subscribe(val => {
-//          console.log(val)
-//           });
-         }
-    //}
-      
-    );
-    const dynamicModule = NgModule({
-      imports: [CommonModule, FormsModule, BrowserModule,ReactiveFormsModule],
-      declarations: [componentOnDemand],
-    })(class DynamicModule {});
-    this.compiler
+        
+      }
+      );
+      const dynamicModule = NgModule({
+        imports: [CommonModule, FormsModule, BrowserModule, ReactiveFormsModule],
+        declarations: [componentOnDemand],
+      })(class DynamicModule {});
+      this.compiler
       .compileModuleAndAllComponentsAsync(dynamicModule)
       .then((factories) => {
         const factory = factories.componentFactories.find(
           (component) => component.componentType === componentOnDemand
-        );
-        this.vf.remove();
-        this.vf.createComponent(factory);
-      });
-  }
+          );
+          this.vf.remove();
+          this.vf.createComponent(factory);
+        });
+      }
   ngAfterViewInit() {}
 }
+
+// if(changes.callComp.currentValue){
+  //   this.callComp = changes.callComp.currentValue;
+
+  //   let secondComp = this.componentFactoryResolver.resolveComponentFactory(DynamicComponent);
+  //  this.compRef= this.vf.createComponent(secondComp);
+  //  //this.compRef.instance.formattedText = 'This is test <element> textInput name="firstInput" required="true" maxLength="4" minLength="2"</element>'
+//  this.compRef.instance.value = 1;
+//   this.compRef.instance.throwVal.subscribe(response=>{alert(response)})
+
+// }else{
+  // this.compRef.destroy();
+  //}
+
+  
+  // validateForm(form: any): boolean {
+    //   for (const name in form.controls) {
+      //     if (form.controls[name] instanceof FormArray) {
+        //       form.controls[name].controls.forEach((formGroup: FormGroup) => {
+          //         this.validateForm(formGroup);
+  //       });
+  //       this.validateForm(form.controls[name]);
+  //     } else if (form.controls[name] instanceof FormGroup) {
+  //       this.validateForm(form.controls[name]);
+  //     } else if (form.controls[name] && form.controls[name].enabled) {
+    //       form.controls[name].updateValueAndValidity();
+    //       form.controls[name].markAsTouched();
+    //       form.controls[name].markAsDirty();
+    //     }
+    //   }
+    //   console.log(form);
+    //   return form.valid;
+  // }
+  
+  //         onValueChanges(): void {
+    // this.testForm.valueChanges.subscribe(val => {
+      //          console.log(val)
+      //           });
+     
+      // this.validateForm(this.testForm)
+      //           if (this.testForm) {
+        //             this.validateForm(this.testForm);
+        //             this.onValueChanges();
+        //}
